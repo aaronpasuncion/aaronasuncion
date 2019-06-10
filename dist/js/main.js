3615b63a502e5d4a6b09b4761522998fb169f617
Aaron P. Asuncion <aaronpasuncion@gmail.com>
2019-06-10 00:36:44 -0400

    added responsiveness to the website

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,23 @@
-//back to top
+$(document).ready(function() {
+  //full navigation menu
+  var $menuBtn = $(".menu-btn");
+  var $menuOverlay = $(".menu-overlay");
 
-var toTop = document.getElementById("toTop");
+  $menuBtn.click(function() {
+    $menuOverlay.fadeToggle(500);
+    $menuOverlay.css({
+      display: "flex"
+    });
+  });
 
-toTop.addEventListener("click", function() {
-  window.scrollTo(0, 0);
+  $menuOverlay.click(function() {
+    $(this).fadeOut(500);
+  });
+  //back to top
+
+  var toTop = document.getElementById("toTop");
+
+  toTop.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+  });
 });
